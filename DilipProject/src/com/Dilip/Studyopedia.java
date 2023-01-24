a7b932acc11db92fb04a2a657676adb4ae2a818d
SubediSb <babu.pkr2@gmail.com>
2023-01-23 21:03:31 -0600

    firstcommit

---
+++ DilipProject/src/com/Dilip/Studyopedia.java
@@ -0,0 +1,12 @@
+package com.Dilip;
+
+public class Studyopedia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+         System.out.println("Hello");
+		
+	}
+
+}
---
+++ java.dilip/src/com/dilip/Studyopedia.java
@@ -0,0 +1,11 @@
+package com.dilip;
+
+public class Studyopedia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.print("First Java Program...Dilip Thinks");
+		
+	}
+
+}
